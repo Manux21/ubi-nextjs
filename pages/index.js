a67620b7a8a36019f4f74442392deff8cc35dfbd
Manux21 <Manux21@yandex.ru>
2022-10-18 21:50:13 +0300

    dynamic routing

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from "next/link";
+
+const Index = () => {
+  return (
+    <div>
+      <div className='navbar'>
+
+        <Link href="/">
+          <a className='link' >Главная</a>
+        </Link>
+
+        <Link href="/users">
+          <a className='link' > Пользователи</a>
+        </Link>
+      </div>
+
+      <h1>Главная страница</h1>
+
+      <style jsx>
+        {`
+          .navbar {
+              background: orange;
+              padding: 15px;
+          }
+          
+          .link {
+              text-decoration: none;
+              color: white;
+              font-size: 20px;
+              margin: 10px;
+              font-family: sans-serif;
+          }
+
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Index;
---
+++ pages/users.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import Link from "next/link";
+
+const Users = () => {
+  const [users, setUsers] = useState([
+    {id: 1, name: 'Petr'},
+    {id: 2, name: 'Vasya'},
+  ])
+
+  return (
+    <div>
+      <h1>Список пользователей</h1>
+      <ul>
+        {users.map(user =>
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>
+              <a>{user.name} </a>
+            </Link>
+          </li>
+          )
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
